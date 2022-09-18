104c858463326de2fb7ca623ae52289acd288d17
ססובר תהילה < @J5>
2022-09-18 16:32:28 +0300

    more functions

---
+++ dotNet5782_9144_5221/TM0/Program5221.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome9144();
+            welcome5221();
+            Console.ReadKey();
+        }
+
+        static partial void welcome9144();
+        private static void welcome5221()
+        {
+            Console.WriteLine();
+            Console.WriteLine("enter your name: ");
+            string name = Console.ReadLine();
+            string family = Console.ReadLine();
+            Console.WriteLine("{0} {1}, welcome to my first application", name, family);
+        }
+
+    }
+}
+
