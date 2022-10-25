9ee8fbe333f63bef3a7abb4f2ffe3e75251839b9
visual studio <vs@mbjcomp.org.il>
2022-10-25 15:22:56 +0300

    restart project

---
+++ dotNet5783_9144_5221/Targil0/Program9144.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Targil0;
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        welcome9144();
+        welcome5221();
+        Console.ReadKey();
+    }
+
+    private static void welcome9144()
+    {
+        string name;
+        Console.Write("Enter your name: ");
+        name = Console.ReadLine();
+        Console.Write(name + " welcome to my first console application");
+    }
+
+    private static void welcome5221()
+    {
+    }
+    }
